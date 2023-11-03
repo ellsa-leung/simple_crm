78a4c2329abf260a35982f5446a5b504e871f43f
ellsa-leung <wmleung74@email.com>
2023-11-03 01:53:50 -0500

    modifies customer partial, add routes, creates customers controller with index, alphabetized and missing email action/view, creates views of index, alphabetized and missing email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resources :customers, only: [:index]
+
+
+  root to: 'customers#index'
+  get 'customers/missing_email'
+  get 'customers/alphabetized'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
